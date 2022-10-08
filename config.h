95a6c6c248614e609a404d2a0534a65f52affff5
Fran <38869988+manudiv16@users.noreply.github.com>
2022-10-08 13:24:35 +0200

    wip

---
+++ config.h
@@ -0,0 +1,28 @@
+/*
+Copyright 2015 Jun Wako <wakojun@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 1
+
+#define MATRIX_COL_PINS { B12 }
+#define MATRIX_ROW_PINS { B14 }
+#define DIODE_DIRECTION COL2ROW
+
+#define DEBOUNCE    5
